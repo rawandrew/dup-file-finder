de83dc95c548d36def2000239468ee92268d1c0f
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-12 16:01:46 +0300

    Fix typo in tests

--- test/duper/results_test.exs
+++ test/duper/results_test.exs
@@ -2,7 +2,7 @@ defmodule Duper.ResultsTest do
   use ExUnit.Case
   alias Duper.Results
 
-  test "can ass entries to the results" do
+  test "can add entries to the results" do
     Results.add_hash_for("path1", 123)
     Results.add_hash_for("path2", 456)
     Results.add_hash_for("path3", 123)
